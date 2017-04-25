23babcc12b1b1b96043a36e41a0d3a46e1a32e98
mgruben <GrubenM@gmail.com>
2017-04-25 09:44:12 -0500

    Remove unused main call

--- src/AmazonRequester.py
+++ src/AmazonRequester.py
@@ -92,13 +92,3 @@ def toFlatFile(listOfTuples):
         
 
 toFlatFile(ret)
-
-if __name__=="__main__":
-    print("To search for a specific item, use, e.g. `response = " + \
-    "amazon.ItemLookup(ItemId=\"B007OZNUCE\")`")
-    
-    print("To search for items by keywords, use, e.g. `response = " +
-    "amazon.ItemSearch(Keywords=\"Kindle 3G\", SearchIndex=\"All\")`")
-    
-    print("To search for similar items, use, e.g., `response = " + 
-    "amazon.SimilarityLookup(ItemId=\"B007OZNUCE\")`")
