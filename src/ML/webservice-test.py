33030518b8f43780f04b11d65286d43aba1142f6
mgruben <GrubenM@gmail.com>
2017-04-26 23:13:25 -0500

    Add pages for results and failure

--- src/ML/webservice-test.py
+++ src/ML/webservice-test.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from MLEngineStub import MLEngine
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("listOfTuples.html")
+
+@app.route("/results")
+def result():
+    if request.method == "POST":
+        return "Hello!"
+    else:
+        return redirect(url_for("no"))
+
+@app.route("/no")
+def no():
+    return "No!"
+
 
 if __name__ == "__main__":
     app.run()
