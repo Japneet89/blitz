6edca5e04a5965b2fe709e8a2a35487c0a8f646b
mgruben <GrubenM@gmail.com>
2017-04-26 22:35:11 -0500

    Describe class functionality

--- src/ML/MLEngineStub.py
+++ src/ML/MLEngineStub.py
@@ -1,1 +1,10 @@
-
+class MLEngine(object):
+    """
+    Accepts lists of tuples during instantiation, performs tf-idf
+    calculations, and returns relevance scores as a list of floats.
+    
+    Ex. can be given ["keywords", tool1, tool2, ..., tool3], where
+    tool1 = ("title", ["feature1", "feature2"], "formatted_price", "ASIN").
+    
+    Could return [0.87, 0.45, ..., 0.98].
+    """
