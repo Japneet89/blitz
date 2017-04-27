cff14048ae795563f456790aeba37c35e0b5653c
mgruben <GrubenM@gmail.com>
2017-04-26 23:52:16 -0500

    Display scores from MLEngine

--- src/ML/webservice-test.py
+++ src/ML/webservice-test.py
@@ -1,26 +1,26 @@
 from flask import Flask, redirect, render_template, request, url_for
 from MLEngineStub import MLEngine
+import ast
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("listOfTuples.html")
+    return render_template("input.html")
 
 @app.route("/result", methods = ["GET", "POST"])
 def result():
     if request.method == "POST":
-        return redirect(url_for("floats"))
+        mle = MLEngine(listOfTuples = ast.literal_eval(request.form["intuple"]))
+        scores = mle.getScores()
+        print(scores)
+        return render_template("display.html", scores = scores)
     else:
         return redirect(url_for("no"))
 
 @app.route("/no")
 def no():
     return "No!"
-
-@app.route("/floats")
-def floats():
-    return "floating!"
 
 if __name__ == "__main__":
     app.run()
