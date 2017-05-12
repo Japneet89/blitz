38a3ea865e5e6be68b7d86dff0a61687060600e5
Kamal Mango <kamal.mango7@gmail.com>
2017-05-13 00:04:49 +0200

    set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
-import './App.css'
-import Login from './Login/login'
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './containers/Login';
+import Account from './containers/Account';
+import Dashboard from './containers/Dashboard';
+
 class App extends Component {
   render () {
     return (
+      <Router>
         <div className='App'>
-          <Login />
+          <Route exact path='/' component={Login} />
+          <Route path='/login' component={Login} />
+          <Route path='/account' component={Account} />
+          <Route path='/dashboard' component={Dashboard} />
         </div>
+      </Router>
     )
   }
 }
---
+++ client/src/containers/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div>
+    <h1>Tools Dashboard</h1>
+  </div>
+)
+
+export default Dashboard;
