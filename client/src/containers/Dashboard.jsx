b4b30cae5cc0ee699019458b1b63738a0048b903
kamalmango <kamal.mango7@gmail.com>
2017-05-17 20:47:19 -0500

    merge branch count

--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import axios from 'axios';
+import Countbox from '../components/Countbox';
+import '../css/Dashboard.css';
 
-const Toolboxes = () => (
-  <div>
-    <h1>Toolboxes</h1>
-  </div>
-)
 
-export default Toolboxes;
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      toolboxes: '',
+      drawers:'',
+      containers:'',
+      tools:''
+    };
+  }
+
+  componentDidMount() {
+    var context = this;
+    axios.get('http://104.154.162.68:8080/api/toolboxes')
+    .then(function (response) {
+      console.log(response);
+      context.setState({toolboxes: response.data.items})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    axios.get('http://104.154.162.68:8080/api/drawers')
+    .then(function (response) {
+      console.log(response);
+      context.setState({drawers: response.data.items})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    axios.get('http://104.154.162.68:8080/api/containers')
+    .then(function (response) {
+      console.log(response);
+      context.setState({containers: response.data.items})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    axios.get('http://104.154.162.68:8080/api/tools')
+    .then(function (response) {
+      console.log(response);
+      context.setState({tools: response.data.items})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="counts">
+        <Countbox itemColor='blue' itemName={'Toolboxes'} itemCount={this.state.toolboxes.length} />
+        <Countbox itemColor='red' itemName={'Drawers'} itemCount={this.state.drawers.length} />
+        <Countbox itemColor='orange' itemName={'Containers'} itemCount={this.state.containers.length} />
+        <Countbox itemColor='green' itemName={'Tools'} itemCount={this.state.tools.length} />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
+
+
