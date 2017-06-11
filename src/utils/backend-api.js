4a59625a1ffebd4025d7133362ed0ba90a204cc4
Shaurav Garg <shauravg@gmail.com>
2017-06-11 11:47:29 -0700

    fixing backend api to return items json array instead of the whole repsonse

--- src/utils/backend-api.js
+++ src/utils/backend-api.js
@@ -4,19 +4,21 @@ import { getAccessToken } from './AuthService';
 const BACKEND_API_URL = 'http://api.tool4dat.com/api';
 
 function getTools() {
-	return axios.get(`${BACKEND_API_URL}/tools`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+	return axios.get(`${BACKEND_API_URL}/tools`, { headers: { Authorization: `Bearer ${getAccessToken()}` }})
+		.then(response => response.data.items)
+		.catch(error => console.log(error));
 }
 
 function getToolboxes() {
-	return axios.get(`${BACKEND_API_URL}/toolboxes`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+	return axios.get(`${BACKEND_API_URL}/toolboxes`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data.items);
 }
 
 function getDrawers() {
-	return axios.get(`${BACKEND_API_URL}/drawers`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+	return axios.get(`${BACKEND_API_URL}/drawers`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data.items);
 }
 
 function getContainers() {
-	return axios.get(`${BACKEND_API_URL}/containers`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+	return axios.get(`${BACKEND_API_URL}/containers`, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data.items);
 }
 
 export {getTools, getToolboxes, getDrawers, getContainers};
