4f0b7955f3a598adfb366b2aecabb10581c186a7
shauravg <shauravg@gmail.com>
2017-10-24 18:18:20 -0700

    temp fix to bug in nodem odule

---
+++ errorsHandler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function rpcError(err, res) {
+
+    if (err.hasOwnProperty('code')) {
+        if (err.code === 404 && !err.hasOwnProperty('message')) {
+            err.message = 'Not found';
+        }
+
+        return res.status(500).json({
+            code:err.code,
+            message:err.message
+        });
+    }
+
+    res.status(500).json({
+        message     : err.message
+    });
+};
+
+
+exports.rpcError = rpcError;
